b4d591932b25a0328157d752bbdb69e141d5d34f
Tzheng456 <Tzheng456@gmail.com>
2021-08-03 19:45:50 +0000

    added delete button to remove urls

--- express_server.js
+++ express_server.js
@@ -56,6 +56,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 // eslint-disable-next-line func-style
 function generateRandomString() {
   let result = "";
